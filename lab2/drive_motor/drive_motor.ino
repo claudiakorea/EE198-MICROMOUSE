4376b19f71a87420a21c7c0ac8d2ff265e2db0b6
Keahooi Hung <keahung@berkeley.edu>
2018-02-15 20:57:13 -0800

    haha i forgot to commit

---
+++ lab2/drive_motor/drive_motor.ino
@@ -0,0 +1,26 @@
+#define PIN_MOTOR_LEFT_1 9
+#define PIN_MOTOR_LEFT_2 10
+#define PIN_MOTOR_RIGHT_1 5
+#define PIN_MOTOR_RIGHT_2 6
+#define PIN_LED1 A3
+#define PIN_LED2 A2
+
+void setup() {
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
+  pinMode(PIN_MOTOR_LEFT_1, OUTPUT);
+  pinMode(PIN_MOTOR_LEFT_2, OUTPUT);
+  pinMode(PIN_MOTOR_RIGHT_1, OUTPUT);
+  pinMode(PIN_MOTOR_RIGHT_2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PIN_LED1, HIGH);
+  digitalWrite(PIN_MOTOR_LEFT_1, HIGH);
+  digitalWrite(PIN_MOTOR_LEFT_2, LOW);
+  delay(1000);
+  digitalWrite(PIN_LED1, LOW);
+  digitalWrite(PIN_LED1, LOW);
+  delay(1000);
+}
+
