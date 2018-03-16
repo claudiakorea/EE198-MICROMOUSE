da82025d3a03d0889dce1aed4d8d00992b0ea326
Keahooi Hung <keahung@berkeley.edu>
2018-03-15 22:23:16 -0700

    sanant

--- mouse/mouse.ino
+++ mouse/mouse.ino
@@ -30,6 +30,9 @@ double velocity_angular_power;
 double velocity_linear_setpoint = 400;
 double velocity_angular_setpoint = 0;
 
+// Wall distance setpoint
+double dist_wall_setpoint = 50;
+
 // DiFfErENTtiAL EQuatIoNs
 double kp_angular = 0.4;
 double kp_linear = 0.004;
@@ -41,10 +44,12 @@ double ki_linear = 0.0005;
 // Derivative
 double kd = 0.00005;
 
-// controllers
+// straight line controllers
 PID pid_linear(&velocity_linear, &velocity_linear_power, &velocity_linear_setpoint, kp_linear, ki_linear, kd, DIRECT);
 PID pid_angular(&velocity_angular, &velocity_angular_power, &velocity_angular_setpoint, kp_angular, ki_angular, kd, DIRECT);
 
+// wall following controllers
+PID pid_dist_wall(&left_dist, &, &dist_wall_setpoint DIRECT);
 
 void setup() {
   Serial.begin(9600);
@@ -74,8 +79,11 @@ void loop() {
   // Your changes should start here //
   ////////////////////////////////////
 
+  
+
   pid_linear.Compute();
   pid_angular.Compute();
+  pid_dist_wall.Compute();
 
   // float left_power = 0.2;
   // float right_power = 0.2;
